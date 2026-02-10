78f0a65dcb9fc2a823edceaab60da610c82f6d4d
Wen16 <weniliu@hotmail.com>
2026-02-10 11:47:10 +1100

    fix: enable touch drag and drop for iOS using mobile-drag-drop polyfill

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { scrollBehaviourDragImageTranslateOverride } from "mobile-drag-drop/scroll-behaviour";
+import { polyfill } from "mobile-drag-drop";
+import "mobile-drag-drop/default.css";
+
+// Initialize mobile-drag-drop polyfill
+polyfill({
+    dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+});
+
 import ErrorBoundary from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
